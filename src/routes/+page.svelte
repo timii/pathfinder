543bf6758ddc31769fb398e78c209194e7e00187
timii <tim-t@hotmail.de>
2023-05-12 22:23:18 +0200

    feat: add tailwind to project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.zinc.900);
+        color: theme(colors.neutral.50);
+    }
+</style>
