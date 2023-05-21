07e89dab0e33f439784fba0e557b3a6d56670a81
timii <tim-t@hotmail.de>
2023-05-21 17:10:17 +0200

    feat: dynamically create fields

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,22 +10,49 @@
     const rows = 5
     const cols = 5
     const numOfFields: number = rows * cols;
-    const fields = new Array<Field>(numOfFields)
-    fields.fill(fieldObject)
-    console.log("fields:", fields)
+    const fields = []
+    const row = new Array<Field[]>(cols)
+    row.fill(fieldObject)
+
+    for(let i = 0; i < rows; i++) {
+        fields.push(row);
+    }
+
+    console.log("fields:", fields, typeof fields, "row:", row, typeof fields[0])
 </script>
 
     <div>Number of fields: {numOfFields}</div>
     
-<h2>Field:</h2>
-{#each fields as field, i}
-    <div>field ({i})</div>
+<h2>Fields:</h2>
+<div class="fields-container">
+
+{#each fields as row, i}
+    <div class="row">
+
+    {#each row as col, j}
+        <div class="field">field ({i})</div>
+    {/each}
+    </div>
 {/each}
+</div>
 
 
 <style lang="postcss">
     :global(html) {
         background-color: theme(colors.zinc.900);
         color: theme(colors.neutral.50);
+    }
+
+    .fields-container {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .row {
+        display: flex;
+    }
+
+    .field {
+        width: 100px;
     }
 </style>
