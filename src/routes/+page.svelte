18d66fd7f6e022ab06e030d8129c0f3bbeee9a53
timii <tim-t@hotmail.de>
2023-06-08 20:53:32 +0200

    fix: remove typing errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,56 @@
 <script lang="ts">
-    import type { Field } from '../types/Field'
+    import { algorithms } from "../algorithms/algorithms";
+    import type { IField } from "../interfaces/Field";
 
-    const fieldObject: Field = {
+    console.log("algorithms:", algorithms, algorithms[0].functionCallback());
+
+    const fieldObject: IField = {
         start: false,
         finish: false,
         wall: false,
-        walkedOver: false
-    }
-    const rows = 10
-    const cols = 10
+        walkedOver: false,
+    };
+    const rows = 10;
+    const cols = 10;
     const numOfFields: number = rows * cols;
-    const fields = []
-    const row = new Array<Field[]>(cols)
-    row.fill(fieldObject)
+    const fields: IField[][] = [];
 
-    for(let i = 0; i < rows; i++) {
+    // create row of fields with 'cols' amount of elements in the row
+    const row: IField[] = new Array<IField>(cols);
+    row.fill(fieldObject);
+
+    for (let i = 0; i < rows; i++) {
         fields.push(row);
     }
 
-    console.log("fields:", fields, typeof fields, "row:", row, typeof fields[0])
+    console.log(
+        "fields:",
+        fields,
+        "row:",
+        row,
+        "total number of fields:",
+        cols * rows
+    );
 </script>
 
+<main
+    class="flex flex-col items-center text-white bg-zinc-900 h-screen font-sans font-lg font-semibold"
+>
+    <div>Number of fields: {numOfFields}</div>
 
-<main class="flex flex-col items-center text-white bg-zinc-900 h-screen font-sans font-lg font-semibold">
-
-<div>Number of fields: {numOfFields}</div>
-    
-<h2>Fields:</h2>
-<div class="flex gap-1">
-
-{#each fields as row, i}
-    <div class="flex flex-col gap-1">
-
-    {#each row as col, j}
-        <div class="field hover:cursor-pointer w-8 h-8 border-solid border-gray-400 border"></div>
-    {/each}
+    <h2>Fields:</h2>
+    <div class="flex gap-1">
+        {#each fields as row, i}
+            <div class="flex flex-col gap-1">
+                {#each row as col, j}
+                    <div
+                        class="field hover:cursor-pointer w-8 h-8 border-solid border-gray-400 border"
+                    />
+                {/each}
+            </div>
+        {/each}
     </div>
-{/each}
-</div>
 </main>
-
 
 <style lang="postcss">
 </style>
