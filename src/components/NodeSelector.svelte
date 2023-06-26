35b75d4d195792b164bd8f597b80f4d08a97453e
timii <tim-t@hotmail.de>
2023-06-26 22:56:07 +0200

    feat: add node selector radio buttons

---
+++ src/components/NodeSelector.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import { selectedNodeType } from "../store/store";
+
+    let selected: string = "wall";
+    selectedNodeType.set(selected);
+
+    function onChange(event: Event) {
+        selected = (event.target as HTMLInputElement).value;
+        selectedNodeType.set(selected);
+    }
+</script>
+
+<div class="">
+    <label>
+        <input
+            checked={selected === "start"}
+            type="radio"
+            on:change={onChange}
+            id="start"
+            name="node-type"
+            value="start"
+        />
+        start
+    </label>
+    <label>
+        <input
+            checked={selected === "finish"}
+            type="radio"
+            on:change={onChange}
+            id="finish"
+            name="node-type"
+            value="finish"
+        />
+        finish
+    </label>
+    <label>
+        <input
+            checked={selected === "wall"}
+            type="radio"
+            on:change={onChange}
+            id="wall"
+            name="node-type"
+            value="wall"
+        />
+        wall
+    </label>
+    <p>Current node type: {$selectedNodeType}</p>
+</div>
+
+<style lang="postcss">
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import Grid from "../components/Grid.svelte";
+    import NodeSelector from "../components/NodeSelector.svelte";
 </script>
 
 <main
     class="flex flex-col items-center text-white bg-zinc-900 h-screen font-sans font-lg font-semibold"
 >
     <Grid />
+
+    <NodeSelector />
 </main>
 
 <style lang="postcss">
