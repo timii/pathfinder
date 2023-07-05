1a7ea9c528cdaa4fb2370f3f28c22f6ce5c592fe
timii <tim-t@hotmail.de>
2023-07-05 21:54:04 +0200

    feat: set fixed start and finish field

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -14,23 +14,43 @@
     const rows = 10;
     const cols = 10;
     const numOfFields: number = rows * cols;
-    const fields: IField[][] = [];
 
-    // create row of fields with 'cols' amount of elements in the row
-    const row: IField[] = new Array<IField>(cols);
-    row.fill(fieldObject);
-
-    for (let i = 0; i < rows; i++) {
-        fields.push(row);
-    }
+    // source: https://stackoverflow.com/a/68966725
+    // create 2d array of fields with cols * rows amount of elements.
+    // it creates a new reference of an array for each index to avoid
+    // that every array is the same reference
+    const fields: IField[][] = Array.from({ length: rows }, (_) =>
+        new Array(cols).fill(fieldObject)
+    );
 
     console.log(
-        "fields:",
+        "fields before:",
         fields,
-        "row:",
-        row,
+        // "row:",
+        // row,
         "total number of fields:",
-        cols * rows
+        cols * rows,
+        "fields[1][1]:",
+        fields[1][1],
+        "fields[8][8]:",
+        fields[8][8]
+    );
+
+    // set a fixed start and finsish field
+    fields[1][1] = { ...fields[1][1], start: true };
+    fields[8][8] = { ...fields[8][8], finish: true };
+
+    console.log(
+        "fields after:",
+        fields,
+        // "row:",
+        // row,
+        "total number of fields:",
+        cols * rows,
+        "fields[1][1]:",
+        fields[1][1],
+        "fields[8][8]:",
+        fields[8][8]
     );
 </script>
 
