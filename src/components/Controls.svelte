fe7ba08f22f0f5c6f4d7909a5a24b1fad7df827e
timii <tim-t@hotmail.de>
2023-07-09 22:14:45 +0200

    feat: add controls component

---
+++ src/components/Controls.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { algorithms } from "../algorithms/algorithms";
+    import { currentGrid, selectedAlgo } from "../store/store";
+
+    console.log("Controls -> selectedAlgo:", $selectedAlgo);
+
+    function startVisualize() {
+        const currentAlgo = algorithms.find((el) => el.name === $selectedAlgo);
+        console.log("startVisualize -> selectedAlgo:", currentAlgo);
+        if (currentAlgo && currentAlgo.functionCallback) {
+            currentAlgo.functionCallback($currentGrid);
+        }
+    }
+</script>
+
+<div>
+    <button on:click={startVisualize} class="bg-white text-black p-2"
+        >Visualize</button
+    >
+</div>
+
+<style lang="postcss">
+</style>
