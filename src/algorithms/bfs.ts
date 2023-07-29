b1ced94c0b1f08b0bdb27b3b6be155da83632b31
timii <tim-t@hotmail.de>
2023-07-29 22:01:08 +0200

    feat: add map to keep track of searches

--- src/algorithms/bfs.ts
+++ src/algorithms/bfs.ts
@@ -1,54 +1,71 @@
 import type { IField } from "../interfaces/Field";
-import type { IPosition } from "../interfaces/Position";
+import type { IPosition, IPositionWithId } from "../interfaces/Position";
 import { currentGrid } from "../store/store";
-import { getAllAdjacentFieldPositions, getFieldPosition, isEveryFieldSearched } from "./utils";
+import { drawShortestPath, getAllAdjacentFieldPositions, getFieldPositionById, getFieldPositionByProp, getShortestPath, isEveryFieldSearched } from "./utils";
 
 // function for the breadth first search algorithm
 export function bfs(grid: IField[][]) {
     console.log("bfs function called -> grid:", grid);
-    const rowMax = grid[0].length
-    const colMax = grid.length
-    const startNode = getFieldPosition(grid, "start")
-    const finishNode = getFieldPosition(grid, "finish")
+    // const rowMax = grid[0].length
+    // const colMax = grid.length
+    const startNode = getFieldPositionByProp(grid, "start")
+    const finishNode = getFieldPositionByProp(grid, "finish")
+    // map to keep track of where we came from (key: next field, value: current field)
+    let cameFromMap = new Map<number, number>()
     if (startNode && finishNode) {
-        let fieldsToCheck: IPosition[] = [startNode]
-        let neighbours: IPosition[] = []
+        let fieldsToCheck: IPositionWithId[] = [startNode]
+        let neighbours: IPositionWithId[] = []
         let i = 0;
 
         const searchInterval = setInterval(() => {
-
-            // while (i < 3) {
-            console.log("in while -> i:", i, "fieldsToCheck:", fieldsToCheck, "neighbours:", neighbours)
+            console.log("in while -> i:", i, "fieldsToCheck:", fieldsToCheck, "neighbours:", neighbours, "cameFromMap:", cameFromMap)
             if (fieldsToCheck.length > 0) {
 
                 // get all the adjacent fields of each field that we have to check
-                fieldsToCheck.forEach(field => neighbours.push(...getAllAdjacentFieldPositions(grid, field.firstIndex, field.secondIndex)))
+                fieldsToCheck.forEach(field => {
+                    neighbours.push(...getAllAdjacentFieldPositions(grid, field.firstIndex, field.secondIndex))
+                    neighbours.forEach(el => {
+                        if (!cameFromMap.has(el.id)) {
+                            cameFromMap.set(el.id, field.id)
+                        }
+                    })
+                    // console.log("in for each -> field:", field, "neighbours:", neighbours)
+                })
 
                 // const neighbours = getAllAdjacentFieldPositions(grid, startNode.firstIndex, startNode.secondIndex)
-                console.log("bfs function called -> startNode:", startNode, "finishNode:", finishNode, "neighbours:", neighbours);
+                console.log("bfs function called -> startNode:", startNode, "finishNode:", finishNode, "neighbours:", neighbours, "cameFromMap:", cameFromMap);
+
                 if (neighbours.length > 0) {
                     fieldsToCheck = []
 
+                    // add neighbours as keys to cameFromMap and use 
+
                     // new fields to check are the neighbours from the field that we checked before
                     fieldsToCheck.push(...neighbours)
-                    neighbours.forEach(field => grid[field.firstIndex][field.secondIndex].searched = true)
+                    neighbours.forEach(field => {
+                        const element = grid[field.firstIndex][field.secondIndex]
+                        if (!element.start && !element.finish)
+                            element.searched = true
+                    })
                 }
-                i++;
                 currentGrid.set(grid)
 
                 console.log("end of interval -> isEveryFieldSearched(grid):", isEveryFieldSearched(grid))
 
-                // stop checking for fields if every field is not empty
-                if (isEveryFieldSearched(grid)) {
+                // stop checking for fields if every field is not empty or a path to the finish field has been found
+                if (isEveryFieldSearched(grid) || cameFromMap.has(finishNode.id)) {
                     clearInterval(searchInterval)
+
+                    // get path from start to finish
+                    const path = getShortestPath(cameFromMap, startNode.id, finishNode.id)
+
+                    // draw path to grid
+                    drawShortestPath(grid, path)
                 }
             }
-            // }
-        }, 1000)
+        }, 500)
     }
 
-    // let gridTest: IField[][] = JSON.parse(JSON.stringify(grid))
-    // grid[0][0].searched = true
     console.log("bfs function called -> grid after:", grid, "isEveryFieldSearched:", isEveryFieldSearched(grid));
 
     return [1, 2, 3];
--- src/interfaces/Position.ts
+++ src/interfaces/Position.ts
@@ -2,3 +2,5 @@ export interface IPosition {
     firstIndex: number,
     secondIndex: number
 }
+
+export type IPositionWithId = IPosition & { id: number }
