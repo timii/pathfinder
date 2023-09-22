70013f99717d6056cd3d6f3de1d5c54042ed60f2
timii <tim-t@hotmail.de>
2023-09-22 22:17:01 +0200

    feat: preload images on site load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,26 @@
     import Grid from "../components/Grid.svelte";
     import Headline from "../components/Headline.svelte";
     import Stats from "../components/Stats.svelte";
+
+    // array of all image urls
+    $: preloadImageUrls = [
+        "close.png",
+        "goal.png",
+        "grass.png",
+        "sand.png",
+        "snow.png",
+        "start.png",
+        "wall.png",
+        "water.png",
+    ];
 </script>
+
+<!-- insert a link for eac image into the head and preload it as soon as the site loads -->
+<svelte:head>
+    {#each preloadImageUrls as image}
+        <link rel="preload" as="image" href={image} />
+    {/each}
+</svelte:head>
 
 <main
     class="flex flex-col items-center text-zinc-100 bg-zinc-900 h-full font-sans font-lg font-semibold p-6"
