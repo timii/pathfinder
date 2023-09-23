68db5e402fc8f6483adabf0181834ff73bcb4440
timii <tim-t@hotmail.de>
2023-09-23 19:33:13 +0200

    fix: add default breakpoints into config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ export default {
     content: ["./src/**/*.{html,js,svelte,ts}"],
     theme: {
         extend: {},
-        screens: { 'customheight': { 'raw': `(min-height: ${CONSTS.statsMinHeight}px)` }, }
+        screens: {
+            'customheight': { 'raw': `(min-height: ${CONSTS.statsMinHeight}px)` },
+            // => @media (min-height: {CONSTS.statsMinHeight}px) { ... }
+
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+        }
     },
     plugins: [],
 };
